b75b8831eee6f03dad2e15dbabea8a7708c09c93
Nuno Pato <nunopato@gmail.com>
2014-02-22 16:03:37 +0000

    adds specs and implementation (view form) to handle user creation

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -7,6 +7,26 @@ describe "User pages" do
   describe "signup page" do
     before { visit signup_path }
 
-    it { should have_content('Sign up') }    
+    let(:submit) { "Create my account" }
+
+    describe "with invalid information" do
+      it "should not create a user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+    end
+
+    describe "with valid information" do
+      before do
+        fill_in "Name",         with: "Gadget Manager"
+        fill_in "Email",        with: "manager@applift.com"
+        fill_in "Password",     with: "gadget"
+        fill_in "Confirmation", with: "gadget"
+      end
+
+      it "should create a user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+    end
+    
   end
 end
